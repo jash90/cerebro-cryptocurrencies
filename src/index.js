316f5489ae518bd0615016286f3c721813feee62
Bartłomiej Zimny <bartekziimny90@gmail.com>
2019-04-05 15:30:50 +0200

    Revert "add currency"
    This reverts commit ddd545baff53be7b3a5e11d691a163ab5129f980.

--- src/index.js
+++ src/index.js
@@ -1,34 +1,52 @@
 import { URL, CURRENCIES, CRYPTOCURRENCIES, API } from "./const";
 import icon from "./icon.png";
-export const fn = ({ term, display }) => {
+export const fn = ({ term, display}) => {
   const regex = /([0-9]+) (\w+) (?:to|in|at) (\w+)/;
   const match = term.toLowerCase().match(regex);
   if (match) {
     let count = Number(match[1]);
     let firstCurrency = String(match[2]).toUpperCase();
     let secondCurrency = String(match[3]).toUpperCase();
+    let cryptocurrency = firstCurrency;
+    let currency = secondCurrency;
+    let divide = false;
 
     if (
-      (CURRENCIES.includes(firstCurrency) ||
-        CRYPTOCURRENCIES.includes(firstCurrency)) &&
-      (CURRENCIES.includes(secondCurrency) ||
-        CRYPTOCURRENCIES.includes(secondCurrency))
+      CURRENCIES.includes(firstCurrency) &&
+      CRYPTOCURRENCIES.includes(secondCurrency)
+    ) {
+      currency = firstCurrency;
+      cryptocurrency = secondCurrency;
+      divide = true;
+    }
+
+    if (
+      CURRENCIES.includes(currency) &&
+      CRYPTOCURRENCIES.includes(cryptocurrency)
     ) {
       try {
-        fetch(`${URL}${firstCurrency}&tsyms=${secondCurrency}&api_key=${API}`)
+        fetch(`${URL}${cryptocurrency}&tsyms=${currency}&api_key=${API}`)
           .then(resp => resp.json())
           .then(response => {
-            const price = Number(response[`${secondCurrency}`]);
-            const value = (count * price).toFixed(2);
+            const price = Number(response[`${currency}`]);
+            const value = this.calculate(divide, count, price);
             display({
               title: `${count} ${firstCurrency} = ${value} ${secondCurrency}`,
-              term: `${term}`,
+              term: `${term} = ${value} ${currency}`,
               icon: icon
             });
           });
       } catch (err) {
+        // do nothing when amount parse failed
         return;
       }
+    }
   }
+};
+export const calculate = (divide, a, b) => {
+  if (divide) {
+    return (a / b).toFixed(2);
+  } else {
+    return (a * b).toFixed(2);
   }
 };
