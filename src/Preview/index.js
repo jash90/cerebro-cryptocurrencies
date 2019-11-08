db436c964f7d95313f839089a1b16f3c64d4cf39
Bartłomiej Zimny <bartlomiejzimny@outlook.com>
2019-11-09 00:58:22 +0100

    c

--- src/Preview/index.js
+++ src/Preview/index.js
@@ -5,7 +5,7 @@ module.exports = ({currency, currencies, thumbnail}) => (
 	<div className={styles.main}>
 		<div className={styles.articleTitleBar}><h1>{currency}</h1></div>
 		<img className={styles.articleThumb} src={thumbnail} />
-        <p>{JSON.stringify(currencies)}</p>
+        <p>{JSON.stringify(Object.keys(currencies))}</p>
 		{Object.keys(currencies).map(key=>{
             <p>`${key}: ${currencies[`${key}`]}`</p>
         })}
