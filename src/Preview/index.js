0689426bf79dc29c89c3887fe22396bec452527c
Bartłomiej Zimny <bartlomiejzimny@outlook.com>
2019-11-09 01:13:28 +0100

    fix

--- src/Preview/index.js
+++ src/Preview/index.js
@@ -1,12 +1,18 @@
 import styles from "./styles.css";
 import React from "react";
 
-module.exports = ({count, currency, currencies, thumbnail}) => (
-	<div className={styles.main}>
-		<div className={styles.articleTitleBar}><h1>{`${count} ${currency}`}</h1></div>
-		<img className={styles.articleThumb} src={thumbnail} />
-		{Object.keys(currencies).map(key=>{
-            <div><h3>{`${key}: ${currencies[`${key}`]*count} ${key}`}</h3></div>
-        })}
-	</div>
-)
+module.exports = ({ count, currency, currencies, thumbnail }) => (
+  <div className={styles.main}>
+    <div className={styles.articleTitleBar}>
+      <h1>{`${count} ${currency}`}</h1>
+    </div>
+    <img className={styles.articleThumb} src={thumbnail} />
+    {Object.keys(currencies).map(key => {
+      return (
+        <div>
+          <h3>{`${key}: ${currencies[`${key}`] * count} ${key}`}</h3>
+        </div>
+      );
+    })}
+  </div>
+);
